d12255e86f8e59e0bc4f7a1af68e87c10531aa9b
benmdeveloper <benyamin_4_ever@yahoo.com>
2015-09-01 02:11:15 +0430

    benyamin1

---
+++ js/index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Benjamin on 9/1/2015.
+ */
+function cir1() {
+    document.getElementById("upper_page").style.backgroundColor = "red";
+}
+function cir2() {
+    document.getElementById("upper_page").style.backgroundColor = "blue";
+}
+function cir3() {
+    document.getElementById("upper_page").style.backgroundColor = "yellow";
+}
