62787dc050d63f7d753ae1bce96412b3223949e9
benmdeveloper <benyamin_4_ever@yahoo.com>
2015-09-01 18:06:18 +0430

    mohammad

--- js/index.js
+++ js/index.js
@@ -17,7 +17,8 @@ function cir2() {
     document.getElementById("cir2").style.borderBottomColor='rgb(105, 52, 26)';
     document.getElementById("cir3").style.backgroundColor='rgb(0,149,132)';
     document.getElementById("cir3").style.borderBottomColor='rgb(0, 105, 89)';
-
+    document.getElementById('l_text1').style.visibility='hidden';
+    document.getElementById('l_text2').style.visibility='visible';
 }
 function cir3() {
     document.getElementById("cir1").style.backgroundColor = "rgb(218,70,56)";
@@ -26,5 +27,6 @@ function cir3() {
     document.getElementById("cir2").style.borderBottomColor='rgb(0, 105, 89)';
     document.getElementById("cir3").style.backgroundColor = "rgb(247,148,29)";
     document.getElementById("cir3").style.borderBottomColor='rgb(105, 52, 26)';
-
+    document.getElementById('l_text1').style.visibility='visible';
+    document.getElementById('l_text2').style.visibility='hidden';
 }
