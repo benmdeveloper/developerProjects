9183c02ec8de4adcf3dbc3233254436666ce8694
benmdeveloper <benyamin_4_ever@yahoo.com>
2015-09-01 18:15:50 +0430

    mohammad

--- js/index.js
+++ js/index.js
@@ -8,7 +8,9 @@ function cir1() {
     document.getElementById("cir2").style.borderBottomColor='rgb(0, 105, 89)';
     document.getElementById("cir3").style.backgroundColor='rgb(218,70,56)';
     document.getElementById("cir3").style.borderBottomColor='rgb(105, 41, 32)';
-
+    document.getElementById('cir1_text').style.visibility='visible';
+    document.getElementById('cir2_text').style.visibility='hidden';
+    document.getElementById('cir3_text').style.visibility='hidden';
 }
 function cir2() {
     document.getElementById("cir1").style.backgroundColor='rgb(218,70,56)';
@@ -17,8 +19,9 @@ function cir2() {
     document.getElementById("cir2").style.borderBottomColor='rgb(105, 52, 26)';
     document.getElementById("cir3").style.backgroundColor='rgb(0,149,132)';
     document.getElementById("cir3").style.borderBottomColor='rgb(0, 105, 89)';
-    document.getElementById('l_text1').style.visibility='hidden';
-    document.getElementById('l_text2').style.visibility='visible';
+    document.getElementById('cir1_text').style.visibility='hidden';
+    document.getElementById('cir2_text').style.visibility='visible';
+    document.getElementById('cir3_text').style.visibility='hidden';
 }
 function cir3() {
     document.getElementById("cir1").style.backgroundColor = "rgb(218,70,56)";
@@ -27,6 +30,7 @@ function cir3() {
     document.getElementById("cir2").style.borderBottomColor='rgb(0, 105, 89)';
     document.getElementById("cir3").style.backgroundColor = "rgb(247,148,29)";
     document.getElementById("cir3").style.borderBottomColor='rgb(105, 52, 26)';
-    document.getElementById('l_text1').style.visibility='visible';
-    document.getElementById('l_text2').style.visibility='hidden';
+    document.getElementById('cir1_text').style.visibility='hidden';
+    document.getElementById('cir2_text').style.visibility='hidden';
+    document.getElementById('cir3_text').style.visibility='visible';
 }
